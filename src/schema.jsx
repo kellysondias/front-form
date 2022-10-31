6b3b9a81c6f722d01fa8139384f417f3840fe5bf
Kellyson <Kellysondias@outlook.com>
2022-10-30 22:44:04 -0300

    Fixing bugs

--- src/schema.jsx
+++ src/schema.jsx
@@ -3,6 +3,5 @@ import * as Yup from 'yup'
 export default Yup.object().shape({
     name: Yup.string().required("Nome é obrigatório"),
     email: Yup.string().email("Este e-mail não é válido").required("E-mail é obrigatório"),
-    phone: Yup.number().min(11, "O número de telefone precisa ter 11 caracteres"),
     cpf: Yup.string().min(11, "O CPF precisa ter 11 caracteres").max(14, "Ultrapassou o limite de caracteres").required("CPF é obrigatório")
 })
